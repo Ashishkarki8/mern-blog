c7a8106e4a71dd7985296d700b818c8c876719f1
Ashish Karki <ashishcarkey89@gmail.com>
2024-12-08 20:15:36 +0545

    created pages and routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter,Routes, Route } from "react-router-dom";  /* alias use gareko while importing */
+import Home from './pages/Home';
+import About from './pages/about';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+
 
 const App = () => {
   return (
-    <div>App</div>
+     <BrowserRouter>
+       <Routes>
+          <Route  path="/" element={<Home />}></Route>        
+          <Route path="/about" element={<About/>}></Route>        
+          <Route path="/sign-in" element={<SignIn />}></Route>  {/* "sign-in" This is the best choice for route naming in REST APIs, as it follows web standards and is SEO-friendly. */}
+          <Route path="/sign-up" element={<SignUp />}></Route>        
+          <Route path="/dashboard" element={<Dashboard />}></Route>        
+          <Route></Route>        
+       </Routes>
+     </BrowserRouter>
+ 
   )
 }
 
